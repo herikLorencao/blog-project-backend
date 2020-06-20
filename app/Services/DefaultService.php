7d1439349b74602cbc887d595e1a01a22d1f101b
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2020-06-20 14:08:32 -0300

    Corrigindo handler de exceção no banco de dados

--- app/Services/DefaultService.php
+++ app/Services/DefaultService.php
@@ -31,8 +31,8 @@ abstract class DefaultService
 
     public function save(array $resourceData)
     {
-            return $this->resourceName::create($resourceData);
         try {
+            return $this->resourceName::create($resourceData);
         } catch (QueryException $e) {
             throw new DatabaseQueryException("Verifique se os dados informados e relacionamentos estão corretos");
         }
